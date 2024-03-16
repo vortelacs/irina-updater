89d7fe31f48e9460ee6e0e1f081be224140d1b3d
Evghenii <evghenii.sirbu@yahoo.com>
2024-03-16 03:00:30 +0200

    Fixed in memory security

--- src/main/java/com/irina/updater/config/security/SecurityConfiguration.java
+++ src/main/java/com/irina/updater/config/security/SecurityConfiguration.java
@@ -3,42 +3,48 @@ package com.irina.updater.config.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Value("${spring.security.user.password}")
     private String adminPassword;
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/admin").permitAll()
-                        .anyRequest().permitAll()
-                );
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/admin/**").authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password(adminPassword)
-                        .roles("admin")
-                        .build();
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode(adminPassword)).roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
     }
 }
