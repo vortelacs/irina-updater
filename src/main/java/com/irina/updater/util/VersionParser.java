aad6e95b7caee01e758baed53eb64bb64d57df0b
Evghenii <evghenii.sirbu@yahoo.com>
2024-03-18 20:45:52 +0200

    Refactored errors in version parser

--- src/main/java/com/irina/updater/util/VersionParser.java
+++ src/main/java/com/irina/updater/util/VersionParser.java
@@ -5,14 +5,14 @@ public class VersionParser {
     public static long parseNumbers(String numbers) {
         String[] segments = numbers.split("\\.");
         if (segments.length != 4) {
-            throw new IllegalArgumentException("Invalid format: must contain four segments");
+            throw new IllegalArgumentException("Invalid format: version have to be of format x.x.x.x ");
         }
 
         long result = 0;
         for (int i = 0; i < 4; i++) {
             int segmentValue = Integer.parseInt(segments[i]);
             if (segmentValue < 0 || segmentValue > 65535) {
-                throw new IllegalArgumentException("Invalid segment value: " + segmentValue);
+                throw new IllegalArgumentException("Invalid segment value: " + segmentValue + ". Should be between 0 and 65535");
             }
             result |= ((long) segmentValue) << ((3 - i) * 16);
         }
