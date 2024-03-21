ab8e478ae48a2727780efd6645dcccd1b7f4afcf
Evghenii <evghenii.sirbu@yahoo.com>
2024-03-21 21:33:29 +0200

    Added parsing json for array feature

--- src/main/java/com/irina/updater/util/JsonUtility.java
+++ src/main/java/com/irina/updater/util/JsonUtility.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtility {
 
@@ -16,6 +18,21 @@ public class JsonUtility {
             return valueNode.asText();
         }
         return null;
+    }
+
+    public static List<String> getArrayValueFromJsonByKey(File jsonFile, String key) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(jsonFile);
+        JsonNode arrayNode = jsonNode.get(key);
+        List<String> values = new ArrayList<>();
+        if (arrayNode != null && arrayNode.isArray()) {
+            for (JsonNode node : arrayNode) {
+                if (node.isTextual()) {
+                    values.add(node.asText());
+                }
+            }
+        }
+        return values;
     }
 
 }
